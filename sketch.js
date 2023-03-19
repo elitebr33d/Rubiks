2d6bfe9158e086bcdd352390b22252656462f4df
elitebr33d <105077317+elitebr33d@users.noreply.github.com>
2023-03-19 05:25:10 -0400

    html rubiks cube

---
+++ sketch.js
@@ -0,0 +1,29 @@
+// sketch.js
+const cubeSize = 50;
+const margin = 10;
+const angleStep = 0.01;
+let angle = 0;
+
+function setup() {
+  createCanvas(600, 400, WEBGL);
+}
+
+function draw() {
+  background(100);
+  rotateX(angle);
+  rotateY(angle);
+
+  for (let x = -cubeSize - margin; x <= cubeSize + margin; x += cubeSize + margin) {
+    for (let y = -cubeSize - margin; y <= cubeSize + margin; y += cubeSize + margin) {
+      for (let z = -cubeSize - margin; z <= cubeSize + margin; z += cubeSize + margin) {
+        push();
+        translate(x, y, z);
+        fill(random(255), random(255), random(255));
+        box(cubeSize);
+        pop();
+      }
+    }
+  }
+
+  angle += angleStep;
+}
